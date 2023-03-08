a373787356c79e5267165b6cf5c18bd2df1fb809
seheeeJeong <usung7888@naver.com>
2023-03-08 19:20:41 +0900

    new project

---
+++ index3.js
@@ -0,0 +1,44 @@
+console.log("연결!");
+
+var name = "홍길동";
+console.log(name);
+
+// 재선언
+var name = "이몽룡";
+console.log(name);
+
+/* let
+1. 재선언 불가. 재할당 가능
+2. 선언과 동시에 초기화하지 않아도 됨 */
+
+let a = 1;
+a = 2;
+console.log(a);
+
+let aa;
+aa = 123;
+
+/* const
+1. 재선언 불가, 재할당 불가
+2. 선언과 동시에 초기화 해야 함*/
+
+const b = 3;
+console.log(b);
+
+let q1 = 2;
+
+q1 = q1 - 2; // 0
+console.log(q1);
+
+const q2 = 5;
+// q2 = q2 - 5;
+
+// 식별자 (변수, 함수)이름 규칙
+// 1. 특수 문자는 $ _ 만 가능, - 불가능
+// 2. 숫자가 맨 처음이면 안됨 ex) 1st 등 안됨
+// 3. 예약어 금지 (let 등)
+let $ = 5;
+console.log($);
+
+let _ = 6;
+console.log(_);
